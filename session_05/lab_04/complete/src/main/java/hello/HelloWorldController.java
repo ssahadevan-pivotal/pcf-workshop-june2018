bf77388bc3a77d10b5f1202279d0ab8b55476093
Rob Winch <rwinch@users.noreply.github.com>
2018-06-19 10:20:30 -0500

    Simplify HelloWorldController

--- session_05/lab_04/complete/src/main/java/hello/HelloWorldController.java
+++ session_05/lab_04/complete/src/main/java/hello/HelloWorldController.java
@@ -2,16 +2,11 @@ package hello;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 public class HelloWorldController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @GetMapping("/hello-world")
-    public Greeting sayHello(@RequestParam(defaultValue="Stranger") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting hello() {
+        return new Greeting(1, "Hello, World!");
     }
 }
